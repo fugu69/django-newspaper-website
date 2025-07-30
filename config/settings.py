cff74736718fcc06ecc4b30bf826c29c8f1f7112
Maximus Brutalis <primeskobelev@gmail.com>
2025-07-30 11:09:16 +0700

    add crispy forms with bootstrap5 styling to signup and login pages

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "pages",
 ]
 
@@ -115,3 +117,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
